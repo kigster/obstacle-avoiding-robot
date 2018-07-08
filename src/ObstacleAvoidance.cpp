6a588f8521922992587fa1463a3be834d205e64a
Konstantin Gredeskoul <kigster@gmail.com>
2018-07-08 02:22:16 -0700

    Reformat + kill unused functions

--- src/ObstacleAvoidance.cpp
+++ src/ObstacleAvoidance.cpp
@@ -55,31 +55,19 @@ char buffer[128];
 
 // blinky shit
 
-void light1(bool on) {
-    led1 = on;
-    led1 ? digitalWrite(ledPin1, HIGH) : digitalWrite(ledPin1, LOW);
+void led_yellow(bool on) {
+  led1 = on;
+  led1 ? digitalWrite(ledPin1, HIGH) : digitalWrite(ledPin1, LOW);
 }
 
-void light2(bool on) {
-    led2 = on;
-    led2 ? digitalWrite(ledPin2, HIGH) : digitalWrite(ledPin2, LOW);
+void led_red(bool on) {
+  led2 = on;
+  led2 ? digitalWrite(ledPin2, HIGH) : digitalWrite(ledPin2, LOW);
 }
 
-void light3(bool on) {
-    led3 = on;
-    led3 ? digitalWrite(ledPin3, HIGH) : digitalWrite(ledPin3, LOW);
-}
-
-void blink1() {
-    light1(!led1);
-}
-
-void blink2() {
-    light2(!led2);
-}
-
-void blink3() {
-    light3(!led3);
+void led_green(bool on) {
+  led3 = on;
+  led3 ? digitalWrite(ledPin3, HIGH) : digitalWrite(ledPin3, LOW);
 }
 
 //_______________________________________________________________________
@@ -87,110 +75,108 @@ void blink3() {
 // Sonar Navigation
 
 unsigned int spaceAhead() {
-    if (millis() > sonarCheckPeriodMs + lastSonarAtMs) {
-        lastSonarAtMs      = millis();
-        unsigned int value = sonar.ping() / US_ROUNDTRIP_CM;
-        if (debug && value > 0 && value < OBSTACLE_DISTANCE_SLOWDOWN) {
-            sprintf(buffer, "spaceAhead is %d", value);
-            bot->log(buffer);
-        }
-        lastSpaceAhead = (value == 0) ? MAX_DISTANCE : value;
+  if (millis() > sonarCheckPeriodMs + lastSonarAtMs) {
+    lastSonarAtMs      = millis();
+    unsigned int value = sonar.ping() / US_ROUNDTRIP_CM;
+    if (debug && value > 0 && value < OBSTACLE_DISTANCE_SLOWDOWN) {
+      sprintf(buffer, "spaceAhead is %d", value);
+      bot->log(buffer);
     }
-    return lastSpaceAhead;
+    lastSpaceAhead = (value == 0) ? MAX_DISTANCE : value;
+  }
+  return lastSpaceAhead;
 }
 
 bool navigateWithSonar() {
-    int distance = spaceAhead();
-    maneuverSpaceAhead = 0;
-    if (distance <= OBSTACLE_DISTANCE_TURN) {
-        turnAndCheck();
-        maneuverSpaceAhead = distance;
-        light1(true);
-        light2(true);
-        light3(false);
-        return true;
-    } else if (distance > OBSTACLE_DISTANCE_TURN
-            && distance < OBSTACLE_DISTANCE_SLOWDOWN) {
-        // reduce speed
-        speed = 100 - (OBSTACLE_DISTANCE_SLOWDOWN - distance) / 2;
-        light1(true);
-        light2(false);
-        light3(false);
-    } else if (distance >= OBSTACLE_DISTANCE_SLOWDOWN) {
-        speed = 100;
-        light1(false);
-        light2(false);
-    }
-    return false;
+  int distance = spaceAhead();
+  maneuverSpaceAhead = 0;
+  if (distance <= OBSTACLE_DISTANCE_TURN) {
+    turnAndCheck();
+    maneuverSpaceAhead = distance;
+    led_yellow(true);
+    led_red(true);
+    led_green(false);
+    return true;
+  } else if (distance > OBSTACLE_DISTANCE_TURN
+             && distance < OBSTACLE_DISTANCE_SLOWDOWN) {
+    // reduce speed
+    speed = 100 - (OBSTACLE_DISTANCE_SLOWDOWN - distance) / 2;
+    led_yellow(true);
+    led_red(false);
+    led_green(false);
+  } else if (distance >= OBSTACLE_DISTANCE_SLOWDOWN) {
+    speed = 100;
+    led_yellow(false);
+    led_red(false);
+  }
+  return false;
 }
 
 void turnAndCheck() {
-    signed short angle = (rand() % 2 == 1) ? 45 : -45;
-    bot->turn(angle, &checkOnce);
+  signed short angle = (rand() % 2 == 1) ? 45 : -45;
+  bot->turn(angle, &checkOnce);
 }
 
 void checkOnce(uint8_t type, signed short parameter) {
-    signed short angle = (parameter == -45) ? 90 : -90;
-    spaceAfterTurn = spaceAhead();
-    if (spaceAfterTurn < maneuverSpaceAhead)
-        bot->turn(angle, &checkTwice);
+  signed short angle = (parameter == -45) ? 90 : -90;
+  spaceAfterTurn = spaceAhead();
+  if (spaceAfterTurn < maneuverSpaceAhead)
+    bot->turn(angle, &checkTwice);
 }
 
 void checkTwice(uint8_t type, signed short parameter) {
-    signed short angle = (parameter == -90) ? -135 : 135;
-    spaceAfterTurn = spaceAhead();
-    if (spaceAfterTurn < maneuverSpaceAhead)
-        bot->turn(angle, NULL);
+  signed short angle = (parameter == -90) ? -135 : 135;
+  spaceAfterTurn = spaceAhead();
+  if (spaceAfterTurn < maneuverSpaceAhead)
+    bot->turn(angle, NULL);
 }
 
 void leftTurn90(uint8_t type, signed short parameter) {
-    delay(1000);
-    bot->turn(-90, &rightTurn90);
+  delay(1000);
+  bot->turn(-90, &rightTurn90);
 }
 
 void rightTurn90(uint8_t type, signed short parameter) {
-    delay(1000);
-    bot->turn(90, &leftTurn90);
+  delay(1000);
+  bot->turn(90, &leftTurn90);
 }
 
 void goBack() {
-    bot->goBackward(70, 600, &turnAround);
+  bot->goBackward(70, 600, &turnAround);
 }
 
 void turnAround(uint8_t type, signed short parameter) {
-    bot->turn(180, NULL);
+  bot->turn(180, NULL);
 }
 
 //______________________________________________________________
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Racer Starting...");
-    srand(millis());
+  Serial.begin(9600);
+  Serial.println("Racer Starting...");
+  srand(millis());
 
-    pinMode(ledPin1, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
-    pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 
-    adapter = new BackSeatDriver_DCMotorAdapter(4, motorLayout);
+  adapter = new BackSeatDriver_DCMotorAdapter(4, motorLayout);
 
-    bot = new BackSeatDriver(adapter);
-    bot->attach();
-    bot->debug(true);
-    bot->setMovingSpeedPercent(70);
-    bot->setTurningDelayCoefficient(6);
-    bot->setTurningSpeedPercent(70);
+  bot = new BackSeatDriver(adapter);
+  bot->attach();
+  bot->debug(true);
+  bot->setMovingSpeedPercent(70);
+  bot->setTurningDelayCoefficient(6);
+  bot->setTurningSpeedPercent(70);
 
-    startMs = millis();
-
+  startMs = millis();
 }
 
 void loop() {
-    if (!bot->isManeuvering() && running) {
-        light3(true);
-        bot->goForward(speed);
-        navigateWithSonar();
-    }
-
+  if (!bot->isManeuvering() && running) {
+    led_green(true);
+    bot->goForward(speed);
+    navigateWithSonar();
+  }
 }
 
--- src/ObstacleAvoidance.h
+++ src/ObstacleAvoidance.h
@@ -2,23 +2,23 @@
 // Created by Konstantin Gredeskoul on 7/8/18.
 //
 
-#ifndef OBSTACLEAVOIDANCE_OBSTACLEAVOIDANCE_H
-#define OBSTACLEAVOIDANCE_OBSTACLEAVOIDANCE_H
+#ifndef OBSTACLEAVOIDANCE_H
+#define OBSTACLEAVOIDANCE_H
 
-void light1(bool on);
-void light2(bool on);
-void light3(bool on);
-void blink1();
-void blink2();
-void blink3();
+void led_yellow(bool on);
+void led_red(bool on);
+void led_green(bool on);
+
 unsigned int spaceAhead();
+
 bool navigateWithSonar();
 void turnAndCheck();
+void goBack();
+
 void checkOnce(uint8_t type, signed short parameter);
 void checkTwice(uint8_t type, signed short parameter);
 void leftTurn90(uint8_t type, signed short parameter);
 void rightTurn90(uint8_t type, signed short parameter);
-void goBack();
 void turnAround(uint8_t type, signed short parameter);
 
-#endif //OBSTACLEAVOIDANCE_OBSTACLEAVOIDANCE_H
+#endif
